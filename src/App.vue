adf945fd424d6ac9267656c86453d92b0dcfd647
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-03 07:59:20 +0700

    3 files modified, 3 added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  <h1>Hello</h1>
+
 <router-view>
 </router-view>
 
@@ -8,17 +8,4 @@
 
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
